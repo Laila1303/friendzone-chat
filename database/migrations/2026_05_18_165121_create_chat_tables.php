aa86285b6c614f4baffc2557ba767be2debc3ec8
Laila1303 <laila.240180177@mhs.unimal.ac.id>
2026-05-18 23:54:43 +0700

    Pembuatan Tabel (Migrations) Chat

---
+++ database/migrations/2026_05_18_165121_create_chat_tables.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(); // untuk nama grup
+            $table->boolean('is_group')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('conversation_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('message');
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('conversation_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('conversation_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conversation_user');
+        Schema::dropIfExists('messages');
+        Schema::dropIfExists('conversations');
+    }
+};
